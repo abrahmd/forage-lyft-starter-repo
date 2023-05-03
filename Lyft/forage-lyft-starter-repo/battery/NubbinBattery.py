3ab72e24b5cda73d81f6eb9b036909f66af321c6
abrahmd <122843912+abrahmd@users.noreply.github.com>
2023-05-03 16:34:09 -0700

    Delete Lyft/forage-lyft-starter-repo directory

--- Lyft/forage-lyft-starter-repo/battery/NubbinBattery.py
+++
@@ -1,9 +0,0 @@
-from battery import Battery
-from datetime import datetime, timedelta
-
-class NubbinBattery(Battery):
-    def __init__(self, current_date, last_service_date):
-        super().__init__(current_date, last_service_date)
-
-    def should_be_serviced(self):
-        return self.current_date - self.last_service_date >= timedelta(years=4)
